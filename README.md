a2d2ec4aa713fe16714d63845700a134c33a6f04
vagrant <vagrant@vagrant.vm>
2019-01-21 06:33:16 +0000

    First commit

---
+++ README.md
@@ -0,0 +1,19 @@
+Welcome to Alec's awesome reporting code :D
+
+There is only one python file needed to run my report.
+From the command line, run the following:
+
+python report.py
+
+Here is what will happen:
+
+1. My file will then scan and collect the necessary data
+    from the 'news' database.
+
+2. If the file 'report.txt' exists, that file will be destroyed
+    and a new file with the same name will be created.
+
+3. It will then write the necessary data into 'report.txt' and Bah-Bahm!
+    You've got your data :)
+
+Loved the project, can't wait for the rest of the course!
---
+++ report.py
@@ -0,0 +1,86 @@
+# This program should do 3 things
+
+# Return the most popular 3 articles in the database and how many views they have.
+# Return the most popular authors and how many views they have.
+# Return the number of days where requests had an error rate of over 1%
+
+import psycopg2
+import pprint as p
+import os
+
+DBNAME = 'news'
+
+def get_top_articles():
+    db = psycopg2.connect(database=DBNAME)
+    c = db.cursor()
+    c.execute("""select articles.title, count(*) as views
+               from articles, log
+               where substring(log.path,10,100) = articles.slug
+               group by articles.title
+               order by views desc
+               limit 3""")
+    results = c.fetchall()
+    db.close()
+    return results
+
+def get_top_authors():
+    db = psycopg2.connect(database=DBNAME)
+    c = db.cursor()
+    c.execute("""select authors.name, sum(query.count) as views
+                  from authors,(select articles.author as id, count(*) as count
+                  from articles, log
+                  where substring(log.path,10,100) = articles.slug
+                  group by articles.id) as query
+                  where authors.id = query.id
+                  group by authors.name
+                  order by views desc""")
+    results = c.fetchall()
+    db.close()
+    return results
+
+def get_error_rate():
+    db = psycopg2.connect(database=DBNAME)
+    c = db.cursor()
+    c.execute("""select base.date::date, base.percent
+                 from (select e.date, e.errors, t.total, round(cast(((e.errors::float / t.total::float) * 100) as numeric),2) as percent
+                 from (select date(time) as date,count(*) as errors
+                 from log
+                 where status != '200 OK'
+                 group by date(time))
+                 as e,
+                 (select date(time), count(*) as total
+                 from log
+                 group by date(time))
+                 as t
+                 where e.date = t.date) as base
+                 where base.percent > 1;
+                 """)
+    results = c.fetchall()
+    db.close()
+    return results
+
+def main():
+
+    # Get data
+    articles = get_top_articles()
+    authors = get_top_authors()
+    errors = get_error_rate()
+
+    # If the report exists, remove it and start from scratch
+    if os.path.exists('report.txt'):
+        os.remove('report.txt')
+
+    # Open new file for report
+    report = open('report.txt','w')
+    report.write("Most popular articles:\n")
+    for i in range(len(articles)):
+        report.write(articles[i][0] + " - " + str(articles[i][1]) + " views\n")
+    report.write("\nMost popular authors:\n")
+    for i in range(len(authors)):
+        report.write(authors[i][0] + " - " + str(authors[i][1]) + " total views\n")
+    report.write("\nDays when 404 errors exceeded 1%:\n")
+    for i in range(len(errors)):
+        report.write(str(errors[i][0]) + " - " + str(errors[i][1]) + "%")
+
+if __name__ == '__main__':
+    main()
